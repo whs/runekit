754dec4aba657195f2c1a840ab8e4be44274e646
Scott Chow <tehchowster@gmail.com>
2021-10-09 17:52:53 -0700

    sc - dont open cors in browser

--- runekit/app/view/browser_window.py
+++ runekit/app/view/browser_window.py
@@ -43,16 +43,17 @@ class PageClass(QWebEnginePage):
         if not is_main_frame:
             return super().acceptNavigationRequest(url, type_, is_main_frame)
 
-        if (
-            type_ != QWebEnginePage.NavigationTypeTyped
-            and url.authority() != self.url().authority()
-        ):
-            # The web is only allowed pages on the same origin
-            # Cross origin pages would open in browser
-            if url.scheme() in ("http", "https"):
-                QDesktopServices.openUrl(url)
+        # This breaks AFK Warden popups
+        # if (
+        #     type_ != QWebEnginePage.NavigationTypeTyped
+        #     and url.authority() != self.url().authority()
+        # ):
+        #     # The web is only allowed pages on the same origin
+        #     # Cross origin pages would open in browser
+        #     if url.scheme() in ("http", "https"):
+        #         QDesktopServices.openUrl(url)
 
-            return False
+        #     return False
 
         return super().acceptNavigationRequest(url, type_, is_main_frame)
 
